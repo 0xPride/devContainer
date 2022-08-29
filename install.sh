2cbac96c2149dce219517bc57bb792b6624893ef
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2022-08-29 14:18:39 +0000

    add better logs

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/ayoubedd/devContainer
+echo "Clonning the config repository"
+git clone https://github.com/ayoubedd/devContainer 1>/dev/null
+
+sleep 1
+echo "installing the config"
 mv devContainer/.devcontainer .
+
+sleep 1
+echo "Cleanning up"
 rm -rf devContainer
-echo "Done cloning config."
+
+sleep 1
+echo "Done installing config."
