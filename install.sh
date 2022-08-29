729a891c64c705390389293cf4edcf222429f7a6
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2022-08-29 14:23:00 +0000

    rediret git clone output to /dev/null

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Clonning the config repository"
-git clone https://github.com/ayoubedd/devContainer 1>/dev/null
+git clone https://github.com/ayoubedd/devContainer 1>/dev/null 2>/dev/null
 
 sleep 1
 echo "installing the config"
